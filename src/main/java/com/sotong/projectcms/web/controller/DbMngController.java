011882534a95b82f4811c96684504aa2b01b4134
kwonkihoon <inyourcare@sotongfive.kr>
2022-09-23 16:36:15 +0900

    h2 DB,JPA 연결 확인

--- src/main/java/com/sotong/projectcms/web/controller/DbMngController.java
+++ src/main/java/com/sotong/projectcms/web/controller/DbMngController.java
@@ -8,8 +8,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
-public class TempController {
-    @RequestMapping(path = "/test", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+public class DbMngController {
+    @RequestMapping(path = "/createDatabase", method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity test() throws Exception {
         return ResponseEntity.ok().body(ApiResponse.builder().success(true).build());
     }
