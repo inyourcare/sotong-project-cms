1c298e976310e611cf2306f8bdcae52a00a540c9
kwonkihoon <inyourcare@sotongfive.kr>
2022-09-23 14:01:05 +0900

    initialcommit

---
+++ src/main/java/com/sotong/projectcms/ProjectCmsApplication.java
@@ -0,0 +1,13 @@
+package com.sotong.projectcms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectCmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectCmsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sotong/projectcms/ProjectCmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sotong.projectcms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectCmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
