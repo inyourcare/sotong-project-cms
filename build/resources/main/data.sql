1628b84ee2fbc5efaeff9dccf49c6d327c0e4fad
kwonkihoon <inyourcare@sotongfive.kr>
2022-09-23 16:49:29 +0900

    clear commit

---
+++ build/resources/main/data.sql
@@ -0,0 +1,4 @@
+INSERT INTO SOTONG_USER VALUES ('bob','1234','bob1','bob@gmail.com');
+INSERT INTO SOTONG_USER VALUES ('jona','1234','jona1','jona@gmail.com');
+INSERT INTO SOTONG_USER VALUES ('kim','1234','kim1','kim@gmail.com');
+INSERT INTO SOTONG_USER VALUES ('lee','1234','lee1','lee@gmail.com');
