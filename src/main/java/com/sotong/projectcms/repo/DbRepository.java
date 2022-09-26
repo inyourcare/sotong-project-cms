2e7a0d0d09f8c9bdfac13e2cf303c6534141efd3
kwonkihoon <inyourcare@sotongfive.kr>
2022-09-26 10:16:48 +0900

    CORS 적용

--- src/main/java/com/sotong/projectcms/repo/DbRepository.java
+++ src/main/java/com/sotong/projectcms/repo/DbRepository.java
@@ -1,29 +1,55 @@
 package com.sotong.projectcms.repo;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowCallbackHandler;
 import org.springframework.stereotype.Repository;
 
-import java.sql.ResultSet;
-import java.sql.SQLException;
+import java.sql.*;
 
 @Repository
 public class DbRepository {
     private final JdbcTemplate jdbcTemplate;
+
+    @Value("${spring.datasource.driverClassName}")
+    private String driverClassName;
+    @Value("${spring.datasource.url}")
+    private String url;
+    @Value("${spring.datasource.username}")
+    private String username;
+    @Value("${spring.datasource.password}")
+    private String password;
 
     @Autowired
     public DbRepository(JdbcTemplate jdbcTemplate) {
         this.jdbcTemplate = jdbcTemplate;
     }
 
-    public void testQuery() {
+    public void testSelectQuery() {
         String sql = """
                 SELECT * from SOTONG_USER
                 """;
         jdbcTemplate.query(sql, new RowCallbackHandler() {
             public void processRow(ResultSet rs) throws SQLException {
                 System.out.println(rs.getString("user_id"));
+            }
+        });
+    }
+    public void testCreateDatabaseQuery() {
+        String sql = "CREATE DATABASE TEST_DATABASE";
+//        System.out.println(driverClassName + url + username + password);
+//        try(Connection conn = DriverManager.getConnection(url, username, password);
+//            Statement stmt = conn.createStatement();
+//        ) {
+//            stmt.executeUpdate(sql);
+//            System.out.println("Database created successfully...");
+//        } catch (SQLException e) {
+//            e.printStackTrace();
+//        }
+        jdbcTemplate.query(sql, new RowCallbackHandler() {
+            public void processRow(ResultSet rs) throws SQLException {
+                System.out.println(rs);
             }
         });
     }
--- src/main/java/com/sotong/projectcms/service/DbMngService.java
+++ src/main/java/com/sotong/projectcms/service/DbMngService.java
@@ -1,15 +1,21 @@
 package com.sotong.projectcms.service;
 
 import com.sotong.projectcms.repo.DbRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class DbMngService {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(DbMngService.class);
+
     @Autowired
     DbRepository dbRepository;
 
     public void testQuery(){
-        dbRepository.testQuery();
+        LOGGER.info("testQuery called");
+//        dbRepository.testCreateDatabaseQuery();
     };
 }
